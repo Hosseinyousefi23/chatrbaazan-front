1c4d38a91e42885c370b6a035c44689ee3ad8c53
mhsharifi96 <mh.sh7676@gmail.com>
2018-12-02 17:33:32 +0330

    contactus service

---
+++ src/app/contactus.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactusService {
+  baseUrl = environment.baseUrl;
+  constructor(private http: HttpClient) { }
+  contactus(data): Observable<any> {
+    return this.http.post(this.baseUrl + 'api/v1/contact/', data);
+  }
+}
