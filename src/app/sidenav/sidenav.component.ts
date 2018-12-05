f3573d8dd478e1ce46ad689b5ea02626b438d2cc
mhsharifi96 <mh.sh7676@gmail.com>
2018-12-05 17:35:06 +0330

    dynamic  sidenav

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { PageService } from '../page.service';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.css']
+  styleUrls: ['./sidenav.component.css'],
+  providers:[PageService]
 })
 export class SidenavComponent implements OnInit {
 
-  constructor() { }
+  public categories: any[] = [];
+
+  constructor(private data: PageService, private user : UsersService) { }
 
   ngOnInit() {
+    this.data.getCategories().subscribe((data :any) => {this.categories = data.data;});
+    
   }
+  
 
 }
