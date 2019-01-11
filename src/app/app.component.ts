67cfebad36aec45c02edaa0ca305cfa7f405e815
mhsharifi96 <mh.sh7676@gmail.com>
2019-01-11 18:04:00 +0330

    fix bug google analitcs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
+// import { Router, NavigationEnd } from '@angular/router';
+
+// declare var gtag: Function;
+// import { distinctUntilChanged } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -8,5 +13,27 @@ import { NgxSpinnerService } from 'ngx-spinner';
 })
 export class AppComponent {
   title = 'chatrbaazan-front';
-  
+  // constructor(private router: Router) { }
+    //site : https://stackoverflow.com/questions/37655898/tracking-google-analytics-page-views-in-angular2
+  // ngOnInit() {
+  //   this.router.events.pipe(distinctUntilChanged((previous: any, current: any) => {
+  //      // Subscribe to any `NavigationEnd` events where the url has changed
+  //      if(current instanceof NavigationEnd) {
+  //       return previous.url === current.url;
+  //   }
+  //   return true;
+  //   }).subscribe((x: any) => {
+  //     gtag('config', 'UA-132350923-1', {'page_path': x.url});
+  // }));
+
+      // this.router.events.distinctUntilChanged((previous: any, current: any) => {
+      //     // Subscribe to any `NavigationEnd` events where the url has changed
+      //     if(current instanceof NavigationEnd) {
+      //         return previous.url === current.url;
+      //     }
+      //     return true;
+      // }).subscribe((x: any) => {
+      //     gtag('config', '<%= GOOGLE_ANALYTICS_ID %>', {'page_path': x.url});
+      // });
+  // }
 }
