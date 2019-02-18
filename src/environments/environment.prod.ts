10e11211131c205fb173868c4afa46d9c03e1b0e
hossein <hossein.yousefi23@gmail.com>
2019-02-18 18:04:03 +0330

    changed base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   // baseUrl: 'http://localhost:8000'
-  // baseUrl: 'http://api.chatrbaazan.ir/',
-  baseUrl: '/',
+  baseUrl: 'http://api.chatrbaazan.ir/',
+  // baseUrl: '/',
   // baseUrl: 'https://cors-anywhere.herokuapp.com/http://api.chatrbaazan.ir/',
   mainurl: 'http://chatrbaazan.ir/'
 };
