02d383173a82ca6c87244c94e69914268c13f5f5
hossein <hossein.yousefi23@gmail.com>
2019-02-18 16:31:17 +0330

    changed url to cors-anywhere

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // baseUrl: 'http://localhost:8000'
-  baseUrl: 'http://api.chatrbaazan.ir/',
+  baseUrl: 'https://cors-anywhere.herokuapp.com/http://api.chatrbaazan.ir/',
   mainurl: 'http://chatrbaazan.ir/'
 };
