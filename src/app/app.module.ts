137a08f48e518daa381b75ae59d7b17b9e420723
hossein <hossein.yousefi23@gmail.com>
2019-02-18 15:57:14 +0330

    add docs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { OwlModule } from 'ngx-owl-carousel';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {OwlModule} from 'ngx-owl-carousel';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
 
-import { HttpClientModule } from '@angular/common/http';
-import { ShareButtonModule } from '@ngx-share/button';
+import {HttpClientModule} from '@angular/common/http';
+import {ShareButtonModule} from '@ngx-share/button';
 
 import {
   MatInputModule,
@@ -22,7 +22,7 @@ import {
   MatCardModule,
   MatSidenavModule,
   MatRadioModule,
-  MatFormFieldModule, 
+  MatFormFieldModule,
   MatAutocompleteModule,
   MatStepperModule,
   MatTabsModule,
@@ -32,88 +32,88 @@ import {
   MatMenuModule,
   MatExpansionModule,
   MatNativeDateModule,
-  
-  
-  } from "@angular/material";
 
 
-import {  MatDialogModule,MatDialogRef,MatDialog} from "@angular/material/dialog";
+} from "@angular/material";
 
-import { ClipboardModule } from 'ngx-clipboard';
 
-import { ToastrModule } from 'ngx-toastr';
+import {MatDialogModule, MatDialogRef, MatDialog} from "@angular/material/dialog";
 
-import { CountdownTimerModule } from 'ngx-countdown-timer';
+import {ClipboardModule} from 'ngx-clipboard';
 
-import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import {ToastrModule} from 'ngx-toastr';
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HeaderComponent } from './header/header.component';
-import { CarouselComponent } from './carousel/carousel.component';
-import { OfferComponent } from './offer/offer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FooterComponent } from './footer/footer.component';
-import { ContactusComponent } from './contactus/contactus.component';
-import { AboutusComponent } from './aboutus/aboutus.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-import { AgmCoreModule } from '@agm/core';
-import { LoginModalComponent } from './login-modal/login-modal.component';
-import { SearchComponent } from './search/search.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { ShoppingCardComponent } from './shopping-card/shopping-card.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
+import {CountdownTimerModule} from 'ngx-countdown-timer';
 
-import { NgxGalleryModule } from 'ngx-gallery';
-import { JalaliPipe } from './pipe/jalali.pipe';
-import { Page404Component } from './page404/page404.component';
-import { CategoriesComponent } from './categories/categories.component';
-import { CompanyComponent } from './company/company.component';
-import { UserprofileComponent } from './userprofile/userprofile.component';
+import {InfiniteScrollModule} from 'ngx-infinite-scroll';
 
-import { ScrollingModule } from '@angular/cdk/scrolling';
-import { DetailModalComponent } from './detail-modal/detail-modal.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HeaderComponent} from './header/header.component';
+import {CarouselComponent} from './carousel/carousel.component';
+import {OfferComponent} from './offer/offer.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FooterComponent} from './footer/footer.component';
+import {ContactusComponent} from './contactus/contactus.component';
+import {AboutusComponent} from './aboutus/aboutus.component';
+import {LoginComponent} from './login/login.component';
+import {SignupComponent} from './signup/signup.component';
+import {ForgotPasswordComponent} from './forgot-password/forgot-password.component';
+import {AgmCoreModule} from '@agm/core';
+import {LoginModalComponent} from './login-modal/login-modal.component';
+import {SearchComponent} from './search/search.component';
+import {ProductDetailComponent} from './product-detail/product-detail.component';
+import {ShoppingCardComponent} from './shopping-card/shopping-card.component';
+import {SidenavComponent} from './sidenav/sidenav.component';
+
+import {NgxGalleryModule} from 'ngx-gallery';
+import {JalaliPipe} from './pipe/jalali.pipe';
+import {Page404Component} from './page404/page404.component';
+import {CategoriesComponent} from './categories/categories.component';
+import {CompanyComponent} from './company/company.component';
+import {UserprofileComponent} from './userprofile/userprofile.component';
+
+import {ScrollingModule} from '@angular/cdk/scrolling';
+import {DetailModalComponent} from './detail-modal/detail-modal.component';
 
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import {CdkTableModule} from '@angular/cdk/table';
 import {CdkTreeModule} from '@angular/cdk/tree';
-import { NgxSpinnerModule } from 'ngx-spinner';
-import { ProductComponent } from './product/product.component';
-import { ConventionComponent } from './convention/convention.component';
-import { BottomSheetOverviewExampleSheet } from './bottom-sheet/bottom-sheet.component';
-import { ApplicationComponent } from './application/application.component';
-import { ResetpasswordconfrimComponent } from './resetpasswordconfrim/resetpasswordconfrim.component';
-import { NgxCaptchaModule } from 'ngx-captcha';
+import {NgxSpinnerModule} from 'ngx-spinner';
+import {ProductComponent} from './product/product.component';
+import {ConventionComponent} from './convention/convention.component';
+import {BottomSheetOverviewExampleSheet} from './bottom-sheet/bottom-sheet.component';
+import {ApplicationComponent} from './application/application.component';
+import {ResetpasswordconfrimComponent} from './resetpasswordconfrim/resetpasswordconfrim.component';
+import {NgxCaptchaModule} from 'ngx-captcha';
 import {GoTopButtonModule} from 'ng2-go-top-button';
-import { SendcodeComponent } from './sendcode/sendcode.component';
-import { FactorComponent } from './factor/factor.component';
+import {SendcodeComponent} from './sendcode/sendcode.component';
+import {FactorComponent} from './factor/factor.component';
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import {  NativeDateAdapter, DateAdapter, MAT_DATE_FORMATS,MAT_DATE_LOCALE} from '@angular/material';
+import {NativeDateAdapter, DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material';
 import * as moment from 'jalali-moment';
-import { Platform } from '@angular/cdk/platform';
-import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
+import {Platform} from '@angular/cdk/platform';
+import {LoadingSpinnerComponent} from './loading-spinner/loading-spinner.component';
 
 
 const MY_DATE_FORMATS = {
   parse: {
-    dateInput: { month: 'short', year: 'numeric', day: 'numeric' }
+    dateInput: {month: 'short', year: 'numeric', day: 'numeric'}
   },
   display: {
     dateInput: 'input',
-    monthYearLabel: { year: 'numeric', month: 'short' },
-    dateA11yLabel: { year: 'numeric', month: 'long', day: 'numeric' },
-    monthYearA11yLabel: { year: 'numeric', month: 'long' }
+    monthYearLabel: {year: 'numeric', month: 'short'},
+    dateA11yLabel: {year: 'numeric', month: 'long', day: 'numeric'},
+    monthYearA11yLabel: {year: 'numeric', month: 'long'}
   }
-}
+};
 
 export class CustomDateAdapter extends NativeDateAdapter {
   constructor(matDateLocale: string) {
     super(matDateLocale, new Platform());
   }
+
   format(date: Date, displayFormat: object): string {
-    var faDate = moment(date.toDateString()).locale('fa').format('YYYY/MM/DD');
-    return faDate;
+    return moment(date.toDateString()).locale('fa').format('YYYY/MM/DD');
   }
 }
 
@@ -149,7 +149,7 @@ export class CustomDateAdapter extends NativeDateAdapter {
     SendcodeComponent,
     FactorComponent,
     LoadingSpinnerComponent,
-    
+
   ],
   imports: [
     BrowserModule,
@@ -199,15 +199,16 @@ export class CustomDateAdapter extends NativeDateAdapter {
     MatMenuModule,
     NgxCaptchaModule,
     GoTopButtonModule,
-    
+
   ],
   // providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   providers: [
-    { provide: MAT_DATE_LOCALE, useValue: 'fa-IR' },
-    { provide: DateAdapter, useClass: CustomDateAdapter, deps: [MAT_DATE_LOCALE] },
-    { provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMATS }
+    {provide: MAT_DATE_LOCALE, useValue: 'fa-IR'},
+    {provide: DateAdapter, useClass: CustomDateAdapter, deps: [MAT_DATE_LOCALE]},
+    {provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMATS}
   ],
   bootstrap: [AppComponent],
-  entryComponents: [LoginModalComponent,DetailModalComponent,BottomSheetOverviewExampleSheet]
+  entryComponents: [LoginModalComponent, DetailModalComponent, BottomSheetOverviewExampleSheet]
 })
-export class AppModule { }
+export class AppModule {
+}
