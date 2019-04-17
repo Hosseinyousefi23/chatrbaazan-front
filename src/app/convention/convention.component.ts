ec1d7e292cdc2031fbfc5e1034966192a1b316f8
hossein <hossein.yousefi23@gmail.com>
2019-04-17 15:41:56 +0430

    middle of smth

--- src/app/convention/convention.component.ts
+++ src/app/convention/convention.component.ts
@@ -1,28 +1,32 @@
-import { LOCAL_STORAGE } from '@ng-toolkit/universal';
-import { Component, OnInit, Input, Output,EventEmitter , Inject} from '@angular/core';
-import { BottomSheetOverviewExampleSheet } from '../bottom-sheet/bottom-sheet.component';
-import { MatBottomSheet } from '@angular/material';
-import { PageService } from '../page.service';
-import { ToastrService } from 'ngx-toastr';
-import { UsersService } from '../users.service';
+import {LOCAL_STORAGE} from '@ng-toolkit/universal';
+import {Component, Inject, Input, OnInit} from '@angular/core';
+import {BottomSheetOverviewExampleSheet} from '../bottom-sheet/bottom-sheet.component';
+import {MatBottomSheet} from '@angular/material';
+import {PageService} from '../page.service';
+import {ToastrService} from 'ngx-toastr';
+import {UsersService} from '../users.service';
 
 declare var $: any;
 
 @Component({
   selector: 'app-convention',
   templateUrl: './convention.component.html',
-  styleUrls: ['./convention.component.css','../sharesCss/shared_style.css']
+  styleUrls: ['./convention.component.css', '../sharesCss/shared_style.css']
 })
 export class ConventionComponent implements OnInit {
 
-  public mostseen : any[] =[];
-  public newest : any[] =[];
-  public mostDiscount: any[] =[];
+  public mostseen: any[] = [];
+  public newest: any[] = [];
+  public mostDiscount: any[] = [];
+  private limit = '20';
   // @Output() showCompo =new EventEmitter<boolean>();
   showDiv = false;
-  @Input() cityHeader :string;
-  constructor(@Inject(LOCAL_STORAGE) private localStorage: any, private offer : PageService ,private user: UsersService,private toastr: ToastrService,
-    private bottomSheet: MatBottomSheet) { }
+  @Input() cityHeader: string;
+
+  constructor(@Inject(LOCAL_STORAGE) private localStorage: any, private offer: PageService, private user: UsersService, private toastr: ToastrService,
+              private bottomSheet: MatBottomSheet) {
+  }
+
   ngOnInit() {
     this.searchoffer();
   }
@@ -30,61 +34,76 @@ export class ConventionComponent implements OnInit {
   ngOnChanges() {
     this.searchoffer();
   }
-  addeventlister(){
-    $(document).ready(function(){
-      $(".card").click(function(){
+
+  addeventlister() {
+    $(document).ready(function () {
+      $(".card").click(function () {
         $(".card").removeClass("voted");
         $(this).addClass("voted");
-        $(".card").find(".offer_image").css("display","block")
-        $(this).find(".offer_image").css("display","none")
+        $(".card").find(".offer_image").css("display", "block")
+        $(this).find(".offer_image").css("display", "none")
 
       });
 
-      $(".back_voted").click(function(e){
-        $(".offer_image").css("display","block")
+      $(".back_voted").click(function (e) {
+        $(".offer_image").css("display", "block")
         e.stopPropagation();
         $(".card").removeClass("voted");
       });
 
-      $(".Copy_btn").click(function(){
+      $(".Copy_btn").click(function () {
         $(this).text("کپی شد");
-      setTimeout( function(){ 
-        $(".Copy_btn").text("کپی")
-      }  , 500 );
+        setTimeout(function () {
+          $(".Copy_btn").text("کپی")
+        }, 500);
       })
 
     });
   }
-  searchoffer(){
-    this.offer.search(null,null,null,'favorites',this.cityHeader,'5',null,'3').subscribe((data :any) => {this.mostseen = data['results'];if(data['results'][0]){this.showDiv =true}; this.addeventlister();});
-    this.offer.search(null,null,null,'created_at',this.cityHeader,'5',null,'3').subscribe((data :any) => { this.newest = data['results']; this.addeventlister();});
-    this.offer.search(null,null,null,'topchatrbazi',this.cityHeader,'5',null,'3').subscribe((data :any) => {this.mostDiscount = data['results']; this.addeventlister();});
+
+  searchoffer() {
+    this.offer.search(null, null, null, 'favorites', this.cityHeader, this.limit, null, '3').subscribe((data: any) => {
+      this.mostseen = data['results'];
+      if (data['results'][0]) {
+        this.showDiv = true
+      }
+      ;this.addeventlister();
+    });
+    this.offer.search(null, null, null, 'created_at', this.cityHeader, this.limit, null, '3').subscribe((data: any) => {
+      this.newest = data['results'];
+      this.addeventlister();
+    });
+    this.offer.search(null, null, null, 'topchatrbazi', this.cityHeader, this.limit, null, '3').subscribe((data: any) => {
+      this.mostDiscount = data['results'];
+      this.addeventlister();
+    });
     this.addeventlister();
   }
-  addtocart(id){
-    if(this.localStorage.getItem("userToken")){
+
+  addtocart(id) {
+    if (this.localStorage.getItem("userToken")) {
       this.user.addtocart(id).subscribe(
-        (data : any) => {
+        (data: any) => {
           // console.log(data)
-          if(data.count && data.count >= 0){
+          if (data.count && data.count >= 0) {
             this.toastr.success('به سبد خرید اضافه شد.')
-          }else{
+          } else {
             // #TODO Handle Error Add To Cart
           }
         }
       )
-      }else{
-        this.toastr.info('ابتدا وارد سایت شوید !!')
-      }
+    } else {
+      this.toastr.info('ابتدا وارد سایت شوید !!')
+    }
   }
 
-  sendfail(slug){
+  sendfail(slug) {
     this.toastr.error('چترتون مستدام ')
     this.offer.sendfailure(slug).subscribe(
     )
   }
 
-  sendclick(product_id){
+  sendclick(product_id) {
     // this.toastr.info('آماده پرتاب')
     this.offer.sendclick_like(product_id).subscribe(
       data => console.log(data)
@@ -97,14 +116,14 @@ export class ConventionComponent implements OnInit {
     )
   }
 
-  finished(a){
-    // 
-    $(".timer_"+a).html('<p style="color:red;">منقضی شد</p>')
+  finished(a) {
+    //
+    $(".timer_" + a).html('<p style="color:red;">منقضی شد</p>')
   }
-  
+
   openBottomSheet(slug): void {
     this.bottomSheet.open(BottomSheetOverviewExampleSheet
-      ,{data:{ 'slug': slug}});
+      , {data: {'slug': slug}});
   }
 
 }
--- src/app/offer/offer.component.ts
+++ src/app/offer/offer.component.ts
@@ -1,29 +1,34 @@
-import { LOCAL_STORAGE } from '@ng-toolkit/universal';
-import { Component, OnInit, Input, Inject, EventEmitter, Output } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
-import { PageService } from '../page.service';
-import { UsersService } from '../users.service';
-import { MatBottomSheetRef, MatBottomSheet, MAT_BOTTOM_SHEET_DATA } from '@angular/material';
-import { BottomSheetOverviewExampleSheet } from '../bottom-sheet/bottom-sheet.component';
+import {LOCAL_STORAGE} from '@ng-toolkit/universal';
+import {Component, Inject, Input, OnInit} from '@angular/core';
+import {ToastrService} from 'ngx-toastr';
+import {PageService} from '../page.service';
+import {UsersService} from '../users.service';
+import {MatBottomSheet} from '@angular/material';
+import {BottomSheetOverviewExampleSheet} from '../bottom-sheet/bottom-sheet.component';
 
 //import * from 'jquery';
 
 declare var $: any;
+
 @Component({
   selector: 'app-offer',
   templateUrl: './offer.component.html',
-  styleUrls: ['./offer.component.css','../sharesCss/shared_style.css']
+  styleUrls: ['./offer.component.css', '../sharesCss/shared_style.css']
 })
 
 export class OfferComponent implements OnInit {
-  public mostseen : any[] =[];
-  public newest : any[] =[];
-  public mostDiscount: any[] =[];
- 
+  public mostseen: any[] = [];
+  public newest: any[] = [];
+  public mostDiscount: any[] = [];
+  private limit = '30';
+
   showDiv = false;
-  @Input() cityHeader :string;
-  constructor(@Inject(LOCAL_STORAGE) private localStorage: any, private offer : PageService ,private user: UsersService,private toastr: ToastrService,
-    private bottomSheet: MatBottomSheet) { }
+  @Input() cityHeader: string;
+
+  constructor(@Inject(LOCAL_STORAGE) private localStorage: any, private offer: PageService, private user: UsersService, private toastr: ToastrService,
+              private bottomSheet: MatBottomSheet) {
+  }
+
   ngOnInit() {
     this.searchoffer();
   }
@@ -31,72 +36,88 @@ export class OfferComponent implements OnInit {
   ngOnChanges() {
     this.searchoffer();
   }
-  addeventlister(){
-    $(document).ready(function(){
-      $(".card").click(function(){
+
+  addeventlister() {
+    $(document).ready(function () {
+      $(".card").click(function () {
         $(".card").removeClass("voted");
         $(this).addClass("voted");
-        $(".card").find(".offer_image").css("display","block")
-        $(this).find(".offer_image").css("display","none")
+        $(".card").find(".offer_image").css("display", "block")
+        $(this).find(".offer_image").css("display", "none")
 
       });
 
-      $(".back_voted").click(function(e){
-        $(".offer_image").css("display","block")
+      $(".back_voted").click(function (e) {
+        $(".offer_image").css("display", "block")
         e.stopPropagation();
         $(".card").removeClass("voted");
       });
-      
-      $(".Copy_btn").click(function(){
+
+      $(".Copy_btn").click(function () {
         $(this).text("کپی شد");
-      setTimeout( function(){ 
-        $(".Copy_btn").text("کپی")
-      }  , 500 );
+        setTimeout(function () {
+          $(".Copy_btn").text("کپی")
+        }, 500);
       })
 
     });
   }
-  searchoffer(){
-    this.offer.search(null,null,null,'favorites',this.cityHeader,'10',null,'4').subscribe((data :any) => {this.mostseen = data['results']; if(data['results'][0]){this.showDiv =true};this.addeventlister();});
-    this.offer.search(null,null,null,'created_at',this.cityHeader,'10',null,'4 ').subscribe((data :any) => { this.newest = data['results']; this.addeventlister();});
-    this.offer.search(null,null,null,'topchatrbazi',this.cityHeader,'10',null,'4').subscribe((data :any) => {this.mostDiscount = data['results']; this.addeventlister();});
+
+  searchoffer() {
+    this.offer.search(null, null, null, 'favorites', this.cityHeader, this.limit, null, '4').subscribe((data: any) => {
+      this.mostseen = data['results'];
+      if (data['results'][0]) {
+        this.showDiv = true
+      }
+      ;this.addeventlister();
+    });
+    this.offer.search(null, null, null, 'created_at', this.cityHeader, this.limit, null, '4 ').subscribe((data: any) => {
+      this.newest = data['results'];
+      this.addeventlister();
+    });
+    this.offer.search(null, null, null, 'topchatrbazi', this.cityHeader, this.limit, null, '4').subscribe((data: any) => {
+      this.mostDiscount = data['results'];
+      this.addeventlister();
+    });
     this.addeventlister();
   }
-  date_timestamp(d){
+
+  date_timestamp(d) {
     let time = new Date(d);
     //console.log(time)
     // console.log(Date.now(),time.getTime());
     // console.log(Date.now()-time.getTime());
-    return Date.now()-time.getTime();
+    return Date.now() - time.getTime();
   }
-  addtocart(id){
+
+  addtocart(id) {
     this.offer.sendclick_like(id).subscribe(
       data => console.log(data)
     )
-    if(this.localStorage.getItem("userToken")){
-    this.user.addtocart(id).subscribe(
-      (data : any) => {
-        // console.log(data)
-        if(data.count && data.count >= 0){
-          this.toastr.success('به سبد خرید اضافه شد.')
-        }else{
-          // #TODO Handle Error Add To Cart
+    if (this.localStorage.getItem("userToken")) {
+      this.user.addtocart(id).subscribe(
+        (data: any) => {
+          // console.log(data)
+          if (data.count && data.count >= 0) {
+            this.toastr.success('به سبد خرید اضافه شد.')
+          } else {
+            // #TODO Handle Error Add To Cart
+          }
         }
-      }
-    )
-    }else{
+      )
+    } else {
       this.toastr.info('ابتدا وارد سایت شوید !!')
     }
   }
 
-  sendfail(slug){
+  sendfail(slug) {
     this.toastr.error('چترتون مستدام ')
     this.offer.sendfailure(slug).subscribe(
       // data => console.log(data)
     )
   }
 
-  sendclick(product_id){
+  sendclick(product_id) {
     // this.toastr.info('آماده پرتاب')
     this.offer.sendclick_like(product_id).subscribe(
       data => console.log(data)
@@ -112,17 +133,17 @@ export class OfferComponent implements OnInit {
     this.offer.sendclick_like(product_id).subscribe(
       // data => console.log(data)
     )
-    
+
   }
 
-  finished(a){
+  finished(a) {
     // $(".timer_"+a).text("منقضی شد")
-    $(".timer_"+a).html('<p style="color:red;">منقضی شد</p>')
+    $(".timer_" + a).html('<p style="color:red;">منقضی شد</p>')
   }
 
   openBottomSheet(slug): void {
     // console.log(slug+"12")
     this.bottomSheet.open(BottomSheetOverviewExampleSheet
-      ,{data:{ 'slug': slug}});
+      , {data: {'slug': slug}});
   }
 }
